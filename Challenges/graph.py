d45914d51bea4635e22d575cd7143d0cf3d377b4
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-07-15 16:59:35 -0700

    challenge1 complete

--- Challenges/graph.py
+++ Challenges/graph.py
@@ -44,7 +44,7 @@ class Vertex(object):
     def get_edge_weight(self, vertex):
         """return the weight of this edge"""
         #  return the weight of the edge from this vertex to the given vertex.
-        return self.neighbors.values()
+        return self.neighbors[vertex]
 
 
 """ Graph Class
@@ -59,6 +59,7 @@ class Graph:
         """
         self.vert_dict = {}
         self.num_vertices = 0
+        self.edge_list = []
 
     def add_vertex(self, key):
         """add a new vertex object to the graph with
@@ -92,7 +93,9 @@ class Graph:
         else:
             # and using the addNeighbor method of the Vertex class.
             # Hint: the vertex f is stored in self.vertList[f].
-            self.vert_dict[key1].add_neighbor(self.vert_dict[key2])
+            self.vert_dict[key1].add_neighbor(self.vert_dict[key2], cost)
+            item = [key1, key2, cost]
+            self.edge_list.append(item)
 
 
     def get_vertices(self):
@@ -108,4 +111,41 @@ class Graph:
 
 # Driver code
 
+if __name__ == "__main__":
+    
+    # Challenge 1: Create the graph
 
+    g = Graph()
+
+    # Add your friends
+    g.add_vertex("Friend 1")
+    g.add_vertex("Friend 2")
+    g.add_vertex("Friend 3")
+    g.add_vertex("Friend 4")
+    g.add_vertex("Friend 5")
+    g.add_vertex("Friend 6")
+    g.add_vertex("Friend 7")
+    g.add_vertex("Friend 8")
+    g.add_vertex("Friend 9")
+    g.add_vertex("Friend 10")
+
+    # ...  add all 10 including you ...
+
+    # Add connections (non weighted edges for now)
+    g.add_edge("Friend 1", "Friend 2")
+    g.add_edge("Friend 2", "Friend 3")
+    g.add_edge("Friend 3", "Friend 4")
+    g.add_edge("Friend 4", "Friend 5")
+    g.add_edge("Friend 5", "Friend 6")
+    g.add_edge("Friend 7", "Friend 8")
+    g.add_edge("Friend 8", "Friend 9")
+    g.add_edge("Friend 9", "Friend 10")
+
+    # Challenge 1: Output the vertices & edges
+    # Print vertices
+    print("The vertices are: ", g.get_vertices(), "\n")
+
+    print("The edges are: ")
+    for v in g:
+        for w in v.get_neighbors():
+            print("( %s , %s )" % (v.get_id(), w.get_id()))
